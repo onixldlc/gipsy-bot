464bc8a3414b0a501581d8864b301b915ed982ed
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-03-01 20:52:13 +0700

    add section read event and event files

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const bot = new Client({ intents :
     [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 
+// read event and event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		bot.once(event.name, (...args) => event.execute(...args));
+	} else {
+		bot.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 // commands collection
 bot.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
