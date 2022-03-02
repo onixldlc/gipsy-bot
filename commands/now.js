0f56f5993e9d75715fe98f5295a18631d7f4f6f7
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-03-03 01:49:13 +0700

    feat: now include timezone

--- commands/now.js
+++ commands/now.js
@@ -6,7 +6,27 @@ module.exports = {
 		const now = new Date();
         const date = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
         const time = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+		var offset =  now.getTimezoneOffset()
+		const timezone = () => {
+			offset /= 60;
 
-        message.channel.send(`\`\`\`now is ${date} ${time} \`\`\``);
+			if(offset == -7)
+				return "WIB";
+			else if(offset == -8)
+				return "WITA";
+			else if(offset == -9)
+				return "WIT";
+			else if(offset == 0) 
+				return "UTC";
+			else if(offset > 0){
+				return "UTC +" + offset;
+			}
+			else if(offset < 0)
+				return "UTC -" + offset;
+			else 
+				return;
+		}
+
+        message.channel.send(`\`\`\`now is ${date} ${time} ${timezone()}\`\`\``);
 	}
 };
