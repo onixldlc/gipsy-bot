bdc6b15d3e0e7f086b8eda53ec3358bbd34ae264
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-03-03 03:22:27 +0700

    feat: ping and add template

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'ping',
+	description: 'checks Bot and API ping',
+	ownerOnly: false,
+    hidden: true,
+	
+	run: (bot, message, args) => {
+		const botp = Date.now() - message.createdTimestamp;
+		const APIp = bot.ws.ping;
+		message.channel.send(`\`\`\`Bot latency: ${botp}ms, API latency: ${APIp}ms\`\`\``);
+	}
+};
