2ad8c4a9de55c33723fd00e3d230c9ab75af52aa
onixldlc <onxdl16427@outlook.com>
2022-03-03 02:25:08 +0700

    added some note to mathfs

--- commands/matfs.js
+++ commands/matfs.js
@@ -7,5 +7,10 @@ module.exports = {
 	checkOwner: false,
 	run: (bot, message, args) => {
 		message.channel.send(`${evaluate(args.join(""))}`)
+		// just fyi gw pas smester 1 kuliah pernah pake bot line gw lupa nama nya tapi dia pake lib ini jg, 
+		// gw sempet main main sama eval function nya, ati ati, sebab gw for some reason waktu itu hampir nemu celah atau bypass
+		// not yet sadly, klo udah gw udh jadiin cve sih :V tpi sayang nya otak smester 1 gw kurang canggih jadi 
+		// just incase watch for any mathjs update
+		
 	}
 };
