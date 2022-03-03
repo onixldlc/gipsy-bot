91755540b52df0b4845028b3a3af0a52eaccb730
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-03-03 15:31:21 +0700

    command now can be categorized in dir

--- utils/loader.js
+++ utils/loader.js
@@ -1,27 +1,35 @@
 const fs = require('node:fs');
+const { promisify } = require('node:util');
+const { glob } = require('glob');
 
-module.exports={
-    // set command to require its respective file
-	hotLoadCommands (bot) {
-		bot.commands.sweep(() => true);
-		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-		for (const file of commandFiles) {
-			delete require.cache[require.resolve(`../commands/${file}`)];
-			const command = require(`../commands/${file}`);
-			bot.commands.set(command.name, command);
+const globPromise = promisify(glob);
+
+// set command to require its respective file recursively
+async function hotLoadCommands (bot) {
+	const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+	commandFiles.map((value) => {
+		const file = require(value);
+		const splitted = value.split("/");
+		const directory = splitted[splitted.length - 2];
+
+		if (file.name) {
+			const properties = { directory, ...file };
+			bot.commands.set(file.name, properties);
 		}
-	},
+	})
+}
 
-    // set event to require its respective file
-    hotLoadEvents (bot) {
-		const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-		for (const file of eventFiles) {
-			const event = require(`../events/${file}`);
-			if (event.once) {
-				bot.once(event.name, (...args) => event.execute(bot, ...args));
-			} else {
-				bot.on(event.name, (...args) => event.execute(bot, ...args));
-			}
+
+function hotLoadEvents (bot) {
+	const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+	for (const file of eventFiles) {
+		const event = require(`../events/${file}`);
+		if (event.once) {
+			bot.once(event.name, (...args) => event.execute(bot, ...args));
+		} else {
+			bot.on(event.name, (...args) => event.execute(bot, ...args));
 		}
 	}
 }
+
+module.exports = { hotLoadCommands, hotLoadEvents };
