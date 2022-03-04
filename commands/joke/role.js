762aaf0fff4ec1c5e8dc30897d0e010abdb3d0d7
Nuzul Abatony <78489357+Albtony@users.noreply.github.com>
2022-03-04 15:10:05 +0700

    polishing...

--- commands/joke/role.js
+++ commands/joke/role.js
@@ -6,15 +6,16 @@ module.exports = {
     hidden: true,
 	
 	run: (bot, message, args) => {
-		const id = args[0].slice(3, args[0].length-1);
-		const member = message.guild.members.cache.get(id);
-
-		if(member.roles.cache.some(role => role.name === 'Chad')){
-			message.reply('**Hi Chad**');
-			message.channel.send('https://tenor.com/view/gigachad-chad-gif-20773266');
-		} else if (member.roles.cache.some(role => role.name === 'beta-male')){
-			message.channel.send('**filthy beta-male**');
-			message.channel.send('https://tenor.com/view/fraz-bradford-meme-world-of-tanks-albania-gif-20568566');
-		}
+		let id, member;
+		let responseArr = [
+			('chad', 'Hi **Chad**' ,'https://tenor.com/view/gigachad-chad-gif-20773266'),
+			('beta-male', 'you filthy **beta-male**', 'https://tenor.com/view/fraz-bradford-meme-world-of-tanks-albania-gif-20568566'),
+			('bros', 'Hello **Bros**', 'https://tenor.com/view/hulk-hogan-nodding-nod-yes-yup-gif-13973219')
+		]
+		
+		if (args) {
+			id = args[0].slice(3, args[0].length-1);
+			member = message.guild.members.cache.get(id);
+		}		
 	}
 };
